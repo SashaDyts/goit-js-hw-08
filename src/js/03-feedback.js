0b8261de06da83e2793b30ccc75ae23ea8737415
SashaDyts <lentrofan@gmail.com>
2022-05-22 20:15:29 +0300

    hw end

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const inputEl = document.querySelector('input');
+const textAreaEl = document.querySelector('textarea');
+const formEl = document.querySelector('.feedback-form');
+
+formEl.addEventListener('input', throttle(onFormElInput, 500));
+formEl.addEventListener('submit', onFormElSubmit);
+
+const data = {};
+
+if (localStorage.length > 0) {
+  const localStorageData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const { email = '', message = '' } = localStorageData;
+
+  inputEl.value = email;
+  textAreaEl.value = message;
+
+  data.email = email;
+  data.message = message;
+}
+
+function onFormElInput(evt) {
+  data[evt.target.name] = evt.target.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+}
+
+function onFormElSubmit(evt) {
+  evt.preventDefault();
+
+  evt.target.reset();
+  localStorage.clear();
+
+  console.log(data);
+}
